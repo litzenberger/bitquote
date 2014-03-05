c3345bfe6e34cfc94972903cee7240facb268a76
ubuntu <ron.litzenberger@gmail.com>
2014-03-06 00:29:03 +0100

    added unit test

--- bitquote.js
+++ bitquote.js
@@ -1,13 +1,17 @@
-//bitquote
-exports.bitquote = function(){
-   var request = require('request');
-var cheerio = require('cheerio');
-request('http://www.coinbase.com', function (error, response, html) {
-  if (!error && response.statusCode == 200) {
-     var $ = cheerio.load(html);
-    $('li.top-balance').each(function(i, element){
-      var a = $(this);
-      console.log(a.text());
+/* Copyright (c) 2014 Ron Litzenberger, MIT License */
+"use strict";
+
+function bitquote(){
+  	var request = require('request');
+  	var cheerio = require('cheerio');
+	request('http://www.coinbase.com', function (error, response, html) {
+  	if (!error && response.statusCode == 200) {
+     	var $ = cheerio.load(html);
+    	$('li.top-balance').each(function(i, element){
+      	var a = $(this);
+      	console.log(a.text());
     });} 
 });
 }
+
+module.exports = bitquote;
---
+++ test/bitquote.test.js
@@ -0,0 +1,2 @@
+var bitquote= require('..');
+bitquote();
